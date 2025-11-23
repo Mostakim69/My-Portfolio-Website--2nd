dfe2ffc13a311311ec167c41fd0a53c69141dda5
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-11-23 19:01:52 +0600

    Added Techstack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HereSection from "@/components/HereSection";
+import TechStack from "@/components/TechStack";
 import WorkExperience from "@/components/WorkExperience";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
       <main className="max-w-7xl mx-auto px-6 lg:px-8">
         <HereSection />
         <WorkExperience />
+        <TechStack />
       </main>
     </>
   );
