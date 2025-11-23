fb8e604d8975240f5acd17ea8d99e47a0e8d6625
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-11-23 11:03:15 +0600

    added Work experiece section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HereSection from "@/components/HereSection";
+import WorkExperience from "@/components/WorkExperience";
 
 const Home = () => {
   return (
     <>
       <main className="max-w-7xl mx-auto px-6 lg:px-8">
         <HereSection />
+        <WorkExperience />
       </main>
     </>
   );
---
+++ src/components/WorkExperience.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkExperience = () => {
+  return (
+    <div>
+      <h1>this work WorkExperience</h1>
+    </div>
+  )
+}
+
+export default WorkExperience
