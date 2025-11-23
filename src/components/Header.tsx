a5210627c870da6e28de5c456141b2754dfb0dff
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-11-23 10:00:43 +0600

    Header Functionlity added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
+"use client";
 import { useEffect, useState } from "react"
 
 const Header = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);    
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-
-    useEffect(() => {
-        const [isScrolled, setIsScrolled] = useState(false);
-        const onScroll = () => setIsScrolled(window.scrollY > 8);    
-    }, []);
-
+  useEffect(() => {
+    const onScroll = () => setIsScrolled(window.scrollY > 8);
+    onScroll();
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <div>
-        <header>Header pages</header>
+      <header>Header pages</header>
     </div>
-  )
-}
+  );
+};
 
 export default Header
